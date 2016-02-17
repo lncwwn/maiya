8461d4af1670f5a50d6018daed9b6587504d5d64
linianchao <lncwwn@gmail.com>
2016-02-17 16:21:51 +0800

    upd: add progress button state

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,14 +10,25 @@
 window.jQuery = window.$ = require('jquery');
 const User = require('./login');
 
-$('body').on('click', '#login-button', function(e) {
+$('body')
+.on('click', '.progress-button', function(e) {
+    e.preventDefault();
+})
+.on('click', '#login-button', function(e) {
+    e.preventDefault();
     const nick = $('#login-form input[name="nick"]').val();
     const password = $('#login-form input[name="password"]').val();
     User.login(nick, password);
-}).on('click', '#register-button', function(e) {
+    $(this).hide();
+    $(this).next().removeClass('uk-hidden');
+})
+.on('click', '#register-button', function(e) {
+    e.preventDefault();
     const nick = $('#register-form input[name="nick"]').val();
     const password1 = $('#register-form input[name="password1"]').val();
     const password2 = $('#register-form input[name="password2"]').val();
     const email = $('#register-form input[name="email"]').val();
     User.register(nick, password1, password2, email);
+    $(this).hide();
+    $(this).next().removeClass('uk-hidden');
 });
