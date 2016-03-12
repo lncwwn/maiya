d635ea35c17e2206b8f9f172ebd2911ec7fbd0bf
linianchao <lncwwn@gmail.com>
2016-03-13 01:05:42 +0800

    upd

--- assets/js/_shop.js
+++ assets/js/_shop.js
@@ -8,6 +8,7 @@
 'use strict';
 
 const util = require('./util');
+const APP_SETTING = require('./app_setting');
 
 module.exports.fetchGoods = function(shopId) {
     const url = `/api/goods/shop/${shopId}`;
@@ -20,5 +21,27 @@ module.exports.showGoods = function(goods) {
         $('#goods-list').html(tip);
         return;
     }
-    // todo
+    let goodsListStr = '',
+        loop = goods.length % 3 === 0 ? goods.length / 3 : parseInt(goods.length / 3) + 1;
+
+    for (let i = 0; i < loop; i++) {
+        let goodsRowStr = '<div class="goods-row uk-width-1-1 uk-grid uk-grid-collapse">';
+        for (let j = i * 3; j < goods.length && j < 3 * (i + 1); j++) {
+            let item = goods[j];
+            const photos = JSON.parse(item.photos);
+            const cover = `${APP_SETTING['qiniu']['goods_url']}/${photos[0]}`;
+            let goodsStr = '<div class="uk-width-1-3">'
+                            + '<div class="goods-item">'
+                            + `<a class="uk-thumbnail" href="/goods/detail/${item.id}">`
+                            + `<img src="${cover}" alt="${item.name}">`
+                            + '</a>'
+                            + `<div class="uk-thumbnail-caption"><a href="/goods/detail/${item.id}">${item.name}</a></div>`
+                            + '</div></div>';
+            goodsRowStr += goodsStr;
+        }
+        goodsRowStr += '</div>';
+        goodsListStr += goodsRowStr;
+    }
+
+    $('#goods-list>.uk-grid').html(goodsListStr);
 };
